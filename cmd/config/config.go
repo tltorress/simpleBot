ed9b3f516e2ef7ea8598bc756ea4b86ee12e43b0
Tomas Torres <tomastorres003@hotmail.com>
2023-12-08 21:18:57 -0300

    fix: reset bot token and hide it

--- cmd/config/config.go
+++ cmd/config/config.go
@@ -1,6 +1,21 @@
 package config
 
-const (
-	BOT_TOKEN = "MTE4MjczODE2MTE0Nzg0Njc4OA.GZcDew.d3NGO4bm_tm4wxW2W9V6a-QYVg9gVKh7MVLKx4"
-	PREFIX    = "!bot"
+import (
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
 )
+
+var BOT_TOKEN string
+
+const PREFIX = "!bot"
+
+func init() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	BOT_TOKEN = os.Getenv("BOT_TOKEN")
+}
